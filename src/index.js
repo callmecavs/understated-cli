f2514bcec91cb5edb0c8a9a8a3e410d7c91e46d5
Michael Cavalea <callmecavs@gmail.com>
2017-07-10 11:16:08 -0400

    add simple Main component

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import {
   h,
   render
 } from 'understated'
+
+const Main = props => (
+  <div class="container">{ props.text }</div>
+)
+
+render(
+  <Main text="Hello, world."/>,
+  document.body
+)
