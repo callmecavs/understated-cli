ae2fb20980167ba2c869e631ebe9a8a1925bd45b
Michael Cavalea <callmecavs@gmail.com>
2017-07-10 10:37:22 -0400

    add simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const isDev = !process.argv.includes('-p')
+
+const config = {
+  context: __dirname + '/src',
+
+  entry: './index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
+
+  cache: isDev,
+
+  devtool: isDev ? 'cheap-module-eval-source-map' : false,
+
+  target: 'web'
+}
